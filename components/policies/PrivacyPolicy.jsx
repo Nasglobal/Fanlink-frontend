a249c549f0b91dc679013a58ef5a7a9bfdb7c68a
Chime Chinonso <66419613+Nasglobal@users.noreply.github.com>
2025-06-19 10:47:32 +0100

    modifying privacy policy font size

--- components/policies/PrivacyPolicy.jsx
+++ components/policies/PrivacyPolicy.jsx
@@ -10,34 +10,34 @@ function PrivacyPolicy() {
      <div className=" "  >  
        <PolicyHeader/>
     
-   <Container>
+   <div className="max-w-[1720px] container mx-auto xl:px-10 md:px-10 2xl:px-8 px-5">
    <div className='mt-8 mb-5 w-full pb-5 border-b'>
     <h2 className='text-primary text-center text-3xl md:text-5xl font-semibold border-gray-950'>Privacy Policy</h2>
     </div>
    
     <div className='flex  flex-col gap-10 text-black'>
-      <p className='text-[16px] leading-5 font-semibold'>Last updated: [06/18/2025]</p>
-      <p className='text-[16px] leading-5'>{"51Lex Fanlink is committed to protecting the privacy of users who use our web application. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you sign up, log in, and use our services. By using 51Lex Fanlink, you agree to the collection and use of information in accordance with this policy."}</p>
+      <p className='text-[16px] xl:text-3xl leading-5 font-semibold'>Last updated: [06/18/2025]</p>
+      <p className='text-[16px] xl:text-xl leading-5'>{"51Lex Fanlink is committed to protecting the privacy of users who use our web application. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you sign up, log in, and use our services. By using 51Lex Fanlink, you agree to the collection and use of information in accordance with this policy."}</p>
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Use of YouTube API Services</p>
-        <p className='text-[16px] leading-5'>51Lex Fanlink uses <span className='font-semibold'>YouTube API Services</span> to enable the generation and management of fanlinks associated with YouTube content.
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>Use of YouTube API Services</p>
+        <p className='text-[16px] xl:text-xl leading-5'>51Lex Fanlink uses <span className='font-semibold'>YouTube API Services</span> to enable the generation and management of fanlinks associated with YouTube content.
 By using our app, you are also agreeing to be bound by the <a className='text-blue-400' href="https://www.youtube.com/t/terms" target="_blank">YouTube Terms of Service</a>{" "}and the <a className='text-blue-400' href="http://www.google.com/policies/privacy" target="_blank">Google Privacy Policy</a>{", "}
   in addition to our own <Link href="/info/terms-and-conditions" className='underline cursor-pointer' >Terms and Conditions</Link>.</p>
       </div>
  
       <div className=''>
-        <p className='md:text-2xl text-lg text-primary font-semibold mb-2'>Information we collect</p>
-        <span className='font-semibold text-gray-950'>-- Personal Information:</span>
-        <ul  className='md:text-[16px] text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
+        <p className='md:text-2xl xl:text-3xl text-lg text-primary font-semibold mb-2'>Information we collect</p>
+        <span className='font-semibold xl:text-xl text-gray-950'>-- Personal Information:</span>
+        <ul  className='md:text-[16px] xl:text-xl text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
           <li> Name </li>
           <li> Email address </li>
           <li> Password </li>
           
         </ul>
         <br/>
-        <span className='font-semibold text-gray-950'>-- Track Information:</span>
-        <ul  className='md:text-[16px] text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
+        <span className='font-semibold xl:text-3xl text-gray-950'>-- Track Information:</span>
+        <ul  className='md:text-[16px] xl:text-xl text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
           <li>Label Name </li>
           <li> Artist Name </li>
           <li> Track Title </li>
@@ -45,8 +45,8 @@ By using our app, you are also agreeing to be bound by the <a className='text-bl
         </ul>
         <br/>
 
-        <span className='font-semibold text-gray-950'>-- Automatically Collected Information:</span>
-        <span className='font-semibold text-gray-950'>Our servers may collect:</span>
+        <span className='font-semibold xl:text-3xl text-gray-950'>-- Automatically Collected Information:</span>
+        <span className='font-semibold xl:text-xl text-gray-950'>Our servers may collect:</span>
         <ul  className='md:text-[16px] text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
           <li>IP Address</li>
           <li> Browser Type and Version </li>
@@ -59,8 +59,8 @@ By using our app, you are also agreeing to be bound by the <a className='text-bl
       
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>How We Use Your Information</p>
-        <ul  className='md:text-[16px] text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>How We Use Your Information</p>
+        <ul  className='md:text-[16px] xl:text-xl text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
           <li>Create and manage your user account</li>
           <li>Enable you to generate and manage fanlinks</li>
           <li>Auto-generate fanlinks for bulk catalogue entries</li>
@@ -71,9 +71,9 @@ By using our app, you are also agreeing to be bound by the <a className='text-bl
 
 
       <div className=''>
-        <p className='md:text-2xl text-lg  text-gray-950 font-semibold mb-2'>Data Sharing and Disclosure</p>
-        <span className='font-semibold text-gray-950'>We do not sell your data. However, we may share data with:</span>
-        <ul  className='md:text-[16px] text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
+        <p className='md:text-2xl text-lg xl:text-3xl text-gray-950 font-semibold mb-2'>Data Sharing and Disclosure</p>
+        <span className='font-semibold xl:text-xl text-gray-950'>We do not sell your data. However, we may share data with:</span>
+        <ul  className='md:text-[16px] xl:text-xl text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
           <li>Google Sheets API, YouTube API, and other integrated third-party APIs</li>
           <li>Legal authorities if required under applicable law</li>
           <li>Service providers assisting in server hosting, analytics, or security</li>
@@ -84,20 +84,20 @@ By using our app, you are also agreeing to be bound by the <a className='text-bl
     
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Data Retention</p>
-        <p className='text-[16px] leading-5'>We retain user and track data for as long as necessary to provide you with the services and comply with legal obligations.</p>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>Data Retention</p>
+        <p className='text-[16px] xl:text-xl leading-5'>We retain user and track data for as long as necessary to provide you with the services and comply with legal obligations.</p>
       </div>
 
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Security</p>
-        <p className='text-[16px] leading-5'>We implement industry-standard security measures, including HTTPS, hashed passwords, and secure APIs to protect your data. </p>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>Security</p>
+        <p className='text-[16px] xl:text-xl leading-5'>We implement industry-standard security measures, including HTTPS, hashed passwords, and secure APIs to protect your data. </p>
       </div>
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Your Rights</p>
-        <span className='font-semibold text-gray-950'>You may:</span>
-        <ul  className='md:text-[16px] text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>Your Rights</p>
+        <span className='font-semibold xl:text-xl text-gray-950'>You may:</span>
+        <ul  className='md:text-[16px] xl:text-xl text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
           <li>Access and update your personal data</li>
           <li>Delete your account (upon request)</li>
           <li>Opt-out of any email communications</li>
@@ -106,26 +106,26 @@ By using our app, you are also agreeing to be bound by the <a className='text-bl
       
       
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>{"Children’s Privacy"}</p>
-        <p className='text-[16px] text-sm leading-5'>51lex Fanlink is not intended for users under the age of 13. We do not knowingly collect data from children.</p>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>{"Children’s Privacy"}</p>
+        <p className='text-[16px] xl:text-xl text-sm leading-5'>51lex Fanlink is not intended for users under the age of 13. We do not knowingly collect data from children.</p>
       </div>
 
     
       
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Changes to This Policy</p>
-        <p className='md:text-[16px] text-sm leading-5'>We may update this Privacy Policy from time to time. Any changes will be posted on this page, and significant changes will be communicated to you via email or notifications.</p>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>Changes to This Policy</p>
+        <p className='md:text-[16px] xl:text-xl text-sm leading-5'>We may update this Privacy Policy from time to time. Any changes will be posted on this page, and significant changes will be communicated to you via email or notifications.</p>
       </div>
 
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Contact Us:</p>
-        <p className='md:text-[16px] text-sm leading-5'>If you have any questions or concerns, contact us at [lola@51lexrecords.com].</p>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>Contact Us:</p>
+        <p className='md:text-[16px] xl:text-xl text-sm leading-5'>If you have any questions or concerns, contact us at [lola@51lexrecords.com].</p>
       </div>
 
     </div>
 
-   </Container>
+   </div>
 
    <footer className="bg-black py-5 mt-10">
     <Container> 
--- components/policies/TermsAndConditions.jsx
+++ components/policies/TermsAndConditions.jsx
@@ -8,7 +8,7 @@ function TermsAndConditions() {
     <div className=" "  >  
        <PolicyHeader/>
     
-   <Container>
+   <div className="max-w-[1720px] container mx-auto xl:px-10 md:px-10 2xl:px-8 px-5">
    <div className='mt-8 mb-5 w-full pb-5 border-b'>
     <h2 className='text-primary text-center text-3xl md:text-5xl font-semibold border-gray-950'>Terms and condition</h2>
     </div>
@@ -17,14 +17,14 @@ function TermsAndConditions() {
       <p className='text-[16px] leading-5'>{"These Terms and Conditions govern your access to and use of 51lex Fanlink. By using our services, you agree to comply with and be bound by these Terms."}</p>
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Acceptance of Terms</p>
-        <p className='text-[16px] leading-5'>By creating an account or using the service, you agree to these Terms and our Privacy Policy. If you do not agree, you may not use the application.</p>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>Acceptance of Terms</p>
+        <p className='text-[16px] xl:text-xl leading-5'>By creating an account or using the service, you agree to these Terms and our Privacy Policy. If you do not agree, you may not use the application.</p>
       </div>
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-primary font-semibold mb-2'>Description of Service</p>
-        <span className='font-semibold text-gray-950'> 51lex Fanlink is a platform that allows registered users to:</span>
-        <ul  className='md:text-[16px] text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
+        <p className='md:text-2xl xl:text-3xl text-lg text-primary font-semibold mb-2'>Description of Service</p>
+        <span className='font-semibold xl:text-xl text-gray-950'> 51lex Fanlink is a platform that allows registered users to:</span>
+        <ul  className='md:text-[16px] xl:text-xl text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
           <li>Create and manage fanlinks for their music tracks</li>
           <li>Automatically generate fanlinks for bulk catalogue entries</li>
           <li>Integrate with platforms such as YouTube, Spotify, Audiomack, Deezer, Boomplay,Itunes,Apple Music and others</li>
@@ -33,9 +33,9 @@ function TermsAndConditions() {
 
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-primary font-semibold mb-2'>User Obligations</p>
-        <span className='font-semibold text-gray-950'>You agree to:</span>
-        <ul  className='md:text-[16px] text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
+        <p className='md:text-2xl xl:text-3xl text-lg text-primary font-semibold mb-2'>User Obligations</p>
+        <span className='font-semibold xl:text-xl text-gray-950'>You agree to:</span>
+        <ul  className='md:text-[16px] xl:text-xl text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
           <li>Provide accurate and truthful information</li>
           <li>Maintain the security of your account and credentials</li>
           <li>Use 51lex Fanlink only for lawful purposes</li>
@@ -45,9 +45,9 @@ function TermsAndConditions() {
 
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-primary font-semibold mb-2'>Account Termination</p>
-        <span className='font-semibold text-gray-950'>We reserve the right to terminate or suspend your account at our discretion, especially for violations such as:</span>
-        <ul  className='md:text-[16px] text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
+        <p className='md:text-2xl xl:text-3xl text-lg text-primary font-semibold mb-2'>Account Termination</p>
+        <span className='font-semibold xl:text-xl text-gray-950'>We reserve the right to terminate or suspend your account at our discretion, especially for violations such as:</span>
+        <ul  className='md:text-[16px] xl:text-xl text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
           <li>Abusing API integrations</li>
           <li>Uploading unauthorized content</li>
           <li>Engaging in fraudulent or malicious activities</li>
@@ -56,27 +56,27 @@ function TermsAndConditions() {
 
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>API Integration & Rate Limits</p>
-        <p className='text-[16px] text-sm leading-5'>51lex Fanlink utilizes third-party APIs (e.g., YouTube Data API, Google Sheets API). Usage is subject to their respective rate limits and terms. Excessive use or violation may result in restricted access.</p>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>API Integration & Rate Limits</p>
+        <p className='text-[16px] xl:text-xl text-sm leading-5'>51lex Fanlink utilizes third-party APIs (e.g., YouTube Data API, Google Sheets API). Usage is subject to their respective rate limits and terms. Excessive use or violation may result in restricted access.</p>
       </div>
 
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Intellectual Property</p>
-        <p className='text-[16px] text-sm leading-5'>All content you upload remains your property. However, you grant Fanlink a non-exclusive license to process and display this content for functionality.</p>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>Intellectual Property</p>
+        <p className='text-[16px] xl:text-xl text-sm leading-5'>All content you upload remains your property. However, you grant Fanlink a non-exclusive license to process and display this content for functionality.</p>
       </div>
 
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Disclaimers</p>
-        <p className='md:text-[16px] text-sm leading-5'>{"51lex Fanlink is provided “as is” without warranties of any kind. We do not guarantee the availability, accuracy, or reliability of third-party links or APIs."}</p>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>Disclaimers</p>
+        <p className='md:text-[16px] xl:text-xl text-sm leading-5'>{"51lex Fanlink is provided “as is” without warranties of any kind. We do not guarantee the availability, accuracy, or reliability of third-party links or APIs."}</p>
       </div>
 
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-primary font-semibold mb-2'>Limitation of Liability</p>
-        <span className='font-semibold text-gray-950'>We are not liable for:</span>
-        <ul  className='md:text-[16px] text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
+        <p className='md:text-2xl xl:text-3xl text-lg text-primary font-semibold mb-2'>Limitation of Liability</p>
+        <span className='font-semibold xl:text-xl text-gray-950'>We are not liable for:</span>
+        <ul  className='md:text-[16px] xl:text-xl text-sm leading-5 md:ps-7 ps-5 mt-2 space-y-3 list-disc '>
           <li>Data loss</li>
           <li>API quota errors or downtime</li>
           <li>Issues arising from incorrect user input</li>
@@ -86,20 +86,20 @@ function TermsAndConditions() {
 
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Modifications to Terms</p>
-        <p className='md:text-[16px] text-sm leading-5'>{"We may modify these Terms at any time. Continued use of 51lex Fanlink after such modifications constitutes your acceptance of the new Terms."}</p>
+        <p className='md:text-2xl xl:text-3xl text-lg text-gray-950 font-semibold mb-2'>Modifications to Terms</p>
+        <p className='md:text-[16px] xl:text-xl text-sm leading-5'>{"We may modify these Terms at any time. Continued use of 51lex Fanlink after such modifications constitutes your acceptance of the new Terms."}</p>
       </div>
 
       <div className=''>
-        <p className='md:text-2xl text-lg text-gray-950 font-semibold mb-2'>Contact Us:</p>
-        <p className='md:text-[16px] text-sm leading-5'>If you have any questions regarding these Terms, please contact us at [lola@51lexrecords.com].</p>
+        <p className='md:text-2xl text-lg xl:text-3xl text-gray-950 font-semibold mb-2'>Contact Us:</p>
+        <p className='md:text-[16px] xl:text-xl text-sm leading-5'>If you have any questions regarding these Terms, please contact us at [lola@51lexrecords.com].</p>
       </div>
 
       
 
     </div>
 
-   </Container>
+   </div>
    <footer className="bg-black py-5 mt-10">
     <Container> 
     <div className="flex flex-col items-center justify-center gap-2 my-4  text-xs text-center text-white">
